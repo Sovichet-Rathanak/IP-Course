96be9453b4ab23a76d3196356b8b707aef1ee4db
Sovichet Rathanak <rathanaksovichet@gmail.com>
2024-11-03 12:57:51 +0700

    Setting up category component

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,99 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
-  <div>This is my first VueJs Projects</div>
-
-  <RouterView />
+  <div class="cat_container">
+    <Category v-for="product in products" 
+      :key="product.label" 
+      :img_src="product.img_src"
+      :label="product.label"
+      :quantity="product.quantity"
+      :bg_color="product.bg_color"
+      :border_color="product.border_color"
+    />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import Category from './components/Category.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  components: {
+    Category
+  },
+  data() {
+    return {
+      products: [
+        {
+          img_src: './src/assets/images/borga.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/peach.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/kiwi.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/apple.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/snacks.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+                {
+          img_src: './src/assets/images/plums.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/vegetables.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/headphones.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/flour.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        },
+        {
+          img_src: './src/assets/images/orange.png',
+          quantity: 30,
+          label: 'Burger',
+          bg_color: '#eab676',
+          border_color: '#e28743'
+        }
+      ]
+    };
   }
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/components/Category.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container" v-bind:style="{backgroundColor: bg_color, borderColor: border_color }">
+    <img :src="img_src" alt="product_image" width="130" height="100">
+    <div class="label">{{ label }}</div>
+    <div class="quantity">{{ quantity }} Items</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img_src: String,
+    quantity: Number,
+    label: String,
+    bg_color: String,
+    border_color: String
+  }
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
+
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 150px;
+  height: 200px;
+  border-radius: 10px;
+  border: 3px solid;
+  margin: .5rem;
+  box-shadow: 10px 5px 15px grey;
+}
+
+.container .label{
+    font-weight: bold;
+    color: black;
+    font-size: 1.5em;
+    margin-top: 1.3rem;
+    font-family: "Roboto Mono", monospace;
+  }
+  
+.container .quantity{
+    font-family: "Roboto Mono", monospace;
+    color: black;
+    font-size: .6rem;
+    margin-top: .7rem;
+}
+</style>
